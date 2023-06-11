9b0edf7e75bb2b52ff8f86c8d1b34fc13ba57596
Ansar Ismagilov <ansar89kab@bk.ru>
2023-06-11 13:42:48 +0800

    hard coded html responce

---
+++ src/main/java/com/kabgig/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,30 @@
+package com.kabgig.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+
+    @RequestMapping("say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello, what are you learning?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My First HTML page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body");
+        sb.append("</body>");
+        sb.append("</html>");
+        return sb.toString();
+    }
+}
